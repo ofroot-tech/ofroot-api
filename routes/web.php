78fef98078c5a2f1ebc1427221b9b2ec55b3932e
ofroot-tech <dimitri.mcdaniel@gmail.com>
2025-10-07 04:10:03 -0400

    saving work: testing routes: adding test route"

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/db-test', function () {
+    try {
+        \DB::connection()->getPdo();
+        return response()->json('Database connection is OK!');
+    } catch (\Exception $e) {
+        return response()->json('Database connection failed: ' . $e->getMessage(), 500);
+    }
+});
+
 Route::get('/health', function () {
     return response()->json(['status' => 'OK'], 200);
 });
